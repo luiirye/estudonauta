76be29ec14b8f8769e3ce3f1d7f4c5a8152a578c
luii <borg.esluii01645@gmail.com>
2026-05-07 14:01:13 -0400

    Primeiro commit

---
+++ python/fundamentos/desafio02.py
@@ -0,0 +1,8 @@
+class Desafio_02:
+    dia = int(input('Digite o dia do seu nascimento:'))
+    mes = int(input('Digite o mês do seu nascimento:'))
+    ano = int(input('Digite o ano do seu nascimento:'))
+    
+    print(
+        f'Você nasceu em {dia}/{mes}/{ano}'
+    )
---
+++ python/fundamentos/desafio03.py
@@ -0,0 +1,6 @@
+class Desafio03:
+    
+    x = int(input('Digite um número inteiro:'))
+    y = int(input('Digite outro número inteiro:'))
+    
+    print(f'A soma de {x} e {y} é = {x + y}')
