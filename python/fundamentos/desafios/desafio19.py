b5ffa583bb3fab804eaa91f11c3574b3a63f0a07
Luiirye <borg.esluii01645@gmail.com>
2026-05-09 23:30:18 -0400

    Desafio 19

--- python/fundamentos/desafios/desafio19.py
+++ python/fundamentos/desafios/desafio19.py
@@ -1,3 +1,8 @@
 # Um professor quer sortear um dos seus quatro alunos para apagar o quadro.
 # Faça um programa que ajude ele, lendo o nome deles e escrevendo o nome escolhido
+import random
+
+nomes = ['Luis Felipe', 'Lara', 'Ruam', 'Ryan']
+sorteado = random.choice(nomes)
+print(f"Aluno sorteado: {sorteado}")
 
