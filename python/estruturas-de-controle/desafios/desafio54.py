6bcb3ccc104e006dda13bcb1a73444030a0ba513
luii <borg.esluii01645@gmail.com>
2026-05-18 18:01:45 -0400

    Exercício 54 iniciado

--- python/estruturas-de-controle/desafios/desafio54.py
+++ python/estruturas-de-controle/desafios/desafio54.py
@@ -1,3 +1,8 @@
 # Crie um programa que leia o ano de nascimento de sete pessoas. 
 # No final, mostre quantas pessoas ainda não atingiram a maioridade e quantas já são maiores de idade.
 # Considere maior idade como 21 anos.
+
+for i in range(1, 8, 1):
+    ano = int(input('informe o ano de nascimento da pessoa {}: ').format(i))
+
+print(ano, end = '')    
