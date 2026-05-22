4ab0abd37f63cb4b72a5ca7d28954609b0f46a2a
luii <borg.esluii01645@gmail.com>
2026-05-22 18:01:59 -0400

    Exercício 66 - Feito

--- python/estruturas-de-controle/desafios/desafio67.py
+++ python/estruturas-de-controle/desafios/desafio67.py
@@ -3,3 +3,27 @@ Faça um programa que mostre:
     - A tabuada de vários números, um de cada vez, para cada valor digitado.
     - O programa será interrompido quando o número digitado for negativo
 '''
+print("==" * 10)
+print("TABUADA DE INTEIROS")
+print("==" * 10)
+
+n = t = 0
+mult = 1
+while True:
+    
+    n = int(input("Digite um número para mostrar sua tabuada: "))
+    t = n * mult
+    
+    if mult <= 10:
+        print(
+            f'{n} * {mult} = {t}'
+        )
+    
+    mult += 1
+    
+    if mult == 10:
+        mult == 1
+    
+    if n < 0:
+        print("Programa encerrado...")
+        break
