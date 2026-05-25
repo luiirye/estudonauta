1bc335c80a81fa26b12806dbedbb469141c496f6
luii <borg.esluii01645@gmail.com>
2026-05-25 17:59:30 -0400

    exercício 67 e 68

--- python/estruturas-de-controle/desafios/desafio67.py
+++ python/estruturas-de-controle/desafios/desafio67.py
@@ -7,23 +7,18 @@ print("==" * 10)
 print("TABUADA DE INTEIROS")
 print("==" * 10)
 
-n = t = 0
-mult = 1
+num = int(input("Informe um número para mostrar sua tabuada: "))
+m = 1 
+
 while True:
     
-    n = int(input("Digite um número para mostrar sua tabuada: "))
-    t = n * mult
+    total = num * m
+    print(f"{num} * {m} = {total}")
+    m += 1
     
-    if mult <= 10:
-        print(
-            f'{n} * {mult} = {t}'
-        )
-    
-    mult += 1
-    
-    if mult == 10:
-        mult == 1
-    
-    if n < 0:
-        print("Programa encerrado...")
-        break
+    if m == 11:
+        m = 1
+        num = int(input("Digite outro número: "))
+        if num < 0:
+            print("Encerrando tabuada...")
+            break
--- python/estruturas-de-controle/desafios/desafio69.py
+++ python/estruturas-de-controle/desafios/desafio69.py
@@ -8,3 +8,38 @@ No final, mostre:
     B) Quantos HOMENS foram cadastrados
     C) Quantas MULHERES tem menos de 20 anos
 '''
+
+
+
+while True:
+    
+    nome = str(input("Informe o nome da pessoa: "))
+    idade = int(input("Informe a idade dessa pessoa: "))
+    sexo = str(input("Informe o sexo dessa pessoa: "))
+    if sexo not in 'MmFf': 
+        while True:
+            print("Sexo inválido. Digite apenas [M/F]")
+            sexo = str(input("Informe o sexo dessa pessoa: "))
+                
+    opt = str(input("Quer continuar? [S/N]: "))    
+    
+    if opt not in 'Ss':
+        print("\n")
+        nome = str(input("Informe o nome da pessoa: "))
+        idade = int(input("Informe a idade dessa pessoa: "))
+        sexo = str(input("Informe o sexo dessa pessoa: "))
+        if sexo not in 'MmFf':
+            while True:
+                print("Sexo inválido. Digite apenas [M/F]")
+                sexo = str(input("Informe o sexo dessa pessoa: "))
+            
+        opt = str(input("Quer continuar? [S/N]: "))
+        
+    else:
+        print("Programa encerrado...")
+        break
+
+print("\n")
+print("Quantidade de pessoas com mais de 18 anos: {}")
+print("Quantidade de Homens cadastrados: {}")
+print("Quantidade de Mulheres com menos de 20 anos cadastradsa: {}")
