78bb879dc253a48c35096fb7180d610a712a159d
luii <borg.esluii01645@gmail.com>
2026-06-12 17:56:29 -0400

    Prints adicionados para melhor leitura desafio 88

--- python/estruturas-compostas/desafios/desafio88.py
+++ python/estruturas-compostas/desafios/desafio88.py
@@ -9,31 +9,33 @@ from time import sleep
 
 jogos = list() # Gera uma lista vazia
 
-print(f'-' * 30)
+print(f'-' * 50)
 print(f'JOGA NA MEGA SENA QUE É SUCESSO!')
-print(f'-' * 30)
+print(f'-' * 50)
 
 palpites = int(input(f'Quantos jogos quer que eu sorteie? '))
+print(f'-' * 50)
 
 print(f'Sorteando {palpites} jogos')
+print(f'-' * 50)
 
 for i in range(palpites):
     jogos.append(list())
 
-print(jogos)
-
 for j in range(palpites):
-    
     for l in range(0,6):
         jogos[j].append(randint(1, 60))
         jogos[j].sort()
 
-
-        
-    
 for k in range(len(jogos)):
+    print(f'Sorteando jogo {k + 1}...')
+    sleep(0.5)
     print(f'Jogo {k + 1:2} : {jogos[k]}')
-    ##sleep()
-    
+    sleep(0.5)
+
+print(f'-' * 50)    
+print(f'Números sorteados para {palpites} jogos!!')
+
+print(f'-' * 50)
 print(f'BOA SORTE!!')
-    
+print(f'-' * 50)
