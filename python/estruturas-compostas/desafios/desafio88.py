fdd301bb1b971b8cfaac33c0c2ec7907ff9d6a17
luii <borg.esluii01645@gmail.com>
2026-06-12 17:26:20 -0400

    Desafio 88 - Implementado Loops para: Adicionar listas em uma lista - Adicionar 6 números aleatórios dentro de cada lista - printar lista por lista

--- python/estruturas-compostas/desafios/desafio88.py
+++ python/estruturas-compostas/desafios/desafio88.py
@@ -3,18 +3,37 @@ Faça um programa que ajude um jogador da MEGA SENA a criar palpites.
 O programa vai perguntar QUANTOS JOGOS serão gerados e vai serotear 6 númeores entre 1 e 60, para cada jogo.
 Cadastre tudo em uma lista composta.
 '''
-palpites = [] # lista para palpites.
 
-while True:
-    
-    quantidade_jogos = int(input('Quantos jogos serão gerados? [Lembrete: cada jogo terá 6 números] '))
-    
-    for i in range(quantidade_jogos):
-        jogos = list()
-    
-    palpites.append(jogos)
-    break
+from random import randint # Para gerar os números aleatórios
+from time import sleep 
 
-print(palpites)
+jogos = list() # Gera uma lista vazia
+
+print(f'-' * 30)
+print(f'JOGA NA MEGA SENA QUE É SUCESSO!')
+print(f'-' * 30)
+
+palpites = int(input(f'Quantos jogos quer que eu sorteie? '))
+
+print(f'Sorteando {palpites} jogos')
+
+for i in range(palpites):
+    jogos.append(list())
+
+print(jogos)
+
+for j in range(palpites):
     
+    for l in range(0,6):
+        jogos[j].append(randint(1, 60))
+        jogos[j].sort()
+
+
+        
+    
+for k in range(len(jogos)):
+    print(f'Jogo {k + 1:2} : {jogos[k]}')
+    ##sleep()
+    
+print(f'BOA SORTE!!')
     
