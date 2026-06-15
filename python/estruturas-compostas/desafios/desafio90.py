d70a89b261bd78b437a22baf814d9771b5a6e3ea
luii <borg.esluii01645@gmail.com>
2026-06-15 17:57:59 -0400

    Exercícios feitos: 90 | Exercícios em andamento: 91, 93, 94, 95

--- python/estruturas-compostas/desafios/desafio90.py
+++ python/estruturas-compostas/desafios/desafio90.py
@@ -3,3 +3,19 @@ Faça um programa que leia NOME E MÉDIA de um aluno.
 Guardando também a situação em um dicionário.
 No final, mostre o conteúdo da estrutura na tela.
 '''
+aluno = dict() # Declaração do dicionário
+aluno['nome'] = str(input(f'Nome: '))
+aluno['media'] = float(input(f'Média de {aluno["nome"]}: '))
+
+# Verificação de situação
+if aluno['media'] >= 7:
+    aluno['situacao'] = 'Aprovado'
+elif aluno['media'] >= 5 and aluno['media'] < 7:
+    aluno['situacao'] = 'Recuperação'
+else:
+    aluno['situacao'] = 'Reprovado'
+
+print(f'-=' * 40)
+for key, value in aluno.items():
+    print(f' -  {key} é igual a {value} ')
+print(f'-=' * 40)
--- python/estruturas-compostas/desafios/desafio91.py
+++ python/estruturas-compostas/desafios/desafio91.py
@@ -3,3 +3,14 @@ Crie um programa onde 4 jogadores joguem um dado e tenham resultados ALEATÓRIOS
 Guarde esses resultados em um dicionário.
 No final, coloque esse dicionário em ORDEM, sabendo que o VENCEDOR tirou O MAIOR NÚMERO no dado.
 '''
+
+from random import randint
+
+jogadores = dict() # Criando o dicionário
+
+# Adicionando jogadores ao dicionário
+for i in range(0,4):
+    jogadores[f"jogador{i}"] = randint(1,6)
+    
+    
+print(jogadores) # Dicionário desordenado.
