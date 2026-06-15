25a3c7420928e4cdea01be8f824911ba5ba3c0f4
luiirye <borg.esluii01645@gmail.com>
2026-06-15 10:43:35 -0400

    ...

---
+++ python/estruturas-compostas/desafios/desafio94.py
@@ -0,0 +1,8 @@
+'''
+Crie um programa que leia NOME, SEXO e IDADE de várias pessoas, guardando os dados de cada pessoa em um dicionário e todos os dicionários em uma lista.
+No final, mostre:
+- A) Quantas pessoas foram cadastradas
+- B) A Média de idade do grupo
+- C) Uma lista com todas as mulheres
+- D) UMa lista com todas as pessoas com idade acima da média
+'''
