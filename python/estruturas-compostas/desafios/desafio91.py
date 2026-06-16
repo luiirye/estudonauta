95e6adb1e948183082eb5ca1360f5980d69fc9cb
luiirye <borg.esluii01645@gmail.com>
2026-06-16 10:41:05 -0400

    Exercícios finalizados: 91| Exercícios em andamento: 92, 93, 94, 95

--- python/estruturas-compostas/desafios/desafio91.py
+++ python/estruturas-compostas/desafios/desafio91.py
@@ -5,12 +5,26 @@ No final, coloque esse dicionário em ORDEM, sabendo que o VENCEDOR tirou O MAIO
 '''
 
 from random import randint
+from time import sleep
+from operator import itemgetter
 
-jogadores = dict() # Criando o dicionário
+jogo = {'Jogador1': randint(1,6),
+        'Jogador2': randint(1,6),
+        'Jogador3': randint(1,6),
+        'Jogador4': randint(1,6)}
 
-# Adicionando jogadores ao dicionário
-for i in range(0,4):
-    jogadores[f"jogador{i}"] = randint(1,6)
+print(' == JOGANDO DADOS == ')
+for chave, valor in jogo.items():
+    print(f'{chave} tirou o {valor} no dado.')
+    sleep(1)
     
-    
-print(jogadores) # Dicionário desordenado.
+ranking = list()
+
+ranking = sorted(jogo.items(), key=itemgetter(1), reverse=True)
+
+print(f'-=' * 30)
+print(f' == RANKING DOS JOGADORES == ')
+for i, valor in enumerate(ranking):
+    print(f'- {i + 1}º Lugar: {valor[0]} com {valor[1]}')
+    sleep(1)
+print()
--- python/estruturas-compostas/desafios/desafio92.py
+++ python/estruturas-compostas/desafios/desafio92.py
@@ -5,3 +5,22 @@ Se por um acaso a CTPS for diferente de ZERO. O dicionário receberá também o 
 Calcule e acrescente, além da IDADE, com quantos anos vai se aposentar.
 Considere aposentadoria 35 anos de contribuição.
 '''
+from datetime import date
+hoje = date.today().year
+print(hoje)
+
+trabalho = dict()
+trabalho['nome'] = str(input(f'Nome: '))
+trabalho['idade'] = int(input(f'Ano de nascimento: '))
+trabalho['idade'] = (hoje - trabalho['idade'])
+trabalho['ctps'] = int(input(f'Carteira de Trabalho (0 = Não tem): '))
+if trabalho['ctps'] == 0:
+     trabalho['ctps'] = 0
+else:
+    trabalho['contratacao'] = int(input(f'Ano de Contratação: '))
+    trabalho['Salario'] = float(int(f'Salário: R$ '))
+    
+    
+    
+for chave, valor in trabalho.items():
+    print(f'{chave} tem o valor {valor}')
