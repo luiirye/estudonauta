c26e3925fc23a2272b0844dfdda37d12a8833232
luiirye <borg.esluii01645@gmail.com>
2026-06-18 09:54:31 -0400

    Desafio 95: Concluído

--- python/estruturas-compostas/desafios/desafio95.py
+++ python/estruturas-compostas/desafios/desafio95.py
@@ -11,24 +11,27 @@ Aprimore o desafio 93 para que ele funcione com vários jogadores, incluindo um 
 # Variáveis 
 jogador = dict() # cadastra UM jogador
 jogadores = list() # guarda TODOS os jogadores cadastrados em jogador
-gols = 0 # variável para armazenar os gols
+gols = list() # lista para armazenar os gols
 gols_soma = 0
 
 while True:
     
     nome = str(input(f'Nome: '))
     partidas = int(input(f'Quantas partidas {nome} jogou? '))
+    for i in range(partidas):
+        gols.append(int(input(f'Quantos gols {nome} fez na {i + 1}ª partida? '))) 
+        gols_soma += gols[i]
+        
+    # adicionando ao dicionário
+    jogador['nome'] = nome
+    jogador['partidas'] = partidas
+    jogador['gols'] = gols[:]
+    jogador['total'] = gols_soma
     
-    for gol in range(partidas):
-        gols = int(input(f'Quantos gols {nome} fez na {gol + 1}ª partida? '))
-        gols_soma += gols
+    jogadores.append(jogador.copy())
     
-    # Adicionando as informações em um dicionário
-    
-    jogador[]
-    jogador[]
-    jogador[]
-    jogador[]
+    gols.clear()
+    gols_soma = 0
     
     resposta = ''
     resposta = str(input(f'Quer continuar? [S/N] '))
@@ -36,4 +39,27 @@ while True:
     if resposta in 'Nn':
         break
         
-print()
+print(f'-=' * 30)
+print(f'<========== FICHA DE JOGADORES ==========>')
+print(f'-=' * 30)
+print(f'Total de jogadores: {len(jogadores)}\nTabela de Jogadores: ')
+
+for i in range(len(jogadores)):
+    print(f'{i + 1} - {jogadores[i]['nome']}')
+    
+while True:
+
+    resposta = ''
+    resposta = str(input(f'Gostaria de ver as estatísticas de algum jogador da tabela? [S/N] '))
+    opt = 0
+    if resposta in 'Ss':
+        print(f'-=' * 30)
+        opt = int(input('Informe o número do jogador na tabela: '))
+        print(f'Ficha de {jogadores[opt - 1]['nome']}')
+        print(f'Partidas jogadas: {jogadores[opt - 1]['partidas']}')
+        print(f'Gols: {jogadores[opt - 1]['gols']}')
+        print(f'Total de gols: {jogadores[opt - 1]['total']}')
+                 
+    elif resposta in 'Nn':
+        break
+print(f'<========== ATÉ MAIS ==========>')
