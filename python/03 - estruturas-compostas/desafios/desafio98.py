1e006cd7e070ceef03956c989a4ac34964768fcf
luiirye <borg.esluii01645@gmail.com>
2026-06-24 17:00:46 -0400

    Exercícios terminados: 98, 99

--- python/03 - estruturas-compostas/desafios/desafio98.py
+++ python/03 - estruturas-compostas/desafios/desafio98.py
@@ -10,12 +10,47 @@ C) Uma contagem personalizada.
 '''
 from time import sleep
 
-def contador(inicio, fim, passo):
-
-    for inicio in range(fim):
-        inicio += passo
-        print(inicio, end = ' ')
+def contador(i, f, p):
+    # Trata o passo caso seja 0
+    if p == 0:
+        p = 1
+    
+    # Exibe a mensagem de início da contagem
+    print('-=' * 20)
+    print(f'Contagem de {i} até {f} de {p} em {p}:')
+    
+    # Trata o passo caso seja negativo (contagem regressiva)
+    if p < 0:
+        p = abs(p)
         
+    # Realiza a contagem progressiva
+    if i < f:
+        cont = i
+        while cont <= f:
+            print(f'{cont} ', end='', flush=True)
+            sleep(0.3)
+            cont += p
+    
+    # Realiza a contagem regressiva
+    else:
+        cont = i
+        while cont >= f:
+            print(f'{cont} ', end='', flush=True)
+            sleep(0.3)
+            cont -= p
+    print('FIM!')
+
+# a) Contagem de 1 até 10, de 1 em 1
 contador(1, 10, 1)
+
+# b) Contagem de 10 até 0, de 2 em 2
 contador(10, 0, 2)
-contador(3,30,10)
+
+# c) Contagem personalizada
+print('-=' * 20)
+print('Agora é a sua vez de personalizar a contagem!')
+ini = int(input('Início: '))
+fim = int(input('Fim:    '))
+pas = int(input('Passo:  '))
+
+contador(ini, fim, pas)
