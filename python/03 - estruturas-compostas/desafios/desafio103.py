045232e24423e214e7601db0c9554b0625510513
luiirye <borg.esluii01645@gmail.com>
2026-07-03 10:40:27 -0400

    Alterações: Exercícios 101, 102, 103

--- python/03 - estruturas-compostas/desafios/desafio103.py
+++ python/03 - estruturas-compostas/desafios/desafio103.py
@@ -4,3 +4,15 @@ Ficha() vai receber dois parâmetros opcionais: o nome de um jogador e quantos g
 
 O Programa deverá ser capaz de mostrar a ficha do jogador, mesmo que algum dado não tenha sido informado corretamente.
 '''
+def ficha(nome = 'Desconhecido', gols = 0):
+    print(f'{nome} fez {gols} gols nesta temporada')
+    
+while True:
+    
+    
+    
+    resposta = ''
+    resposta = str((input(f'Quer continuar? [S/N] ')))
+    if resposta in 'Nn':
+        print(f'Volte sempre!')
+        break
