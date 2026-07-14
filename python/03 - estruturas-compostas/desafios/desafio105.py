275e3f4cfe7dadf13d2216cd8327c04d72565a43
luiirye <borg.esluii01645@gmail.com>
2026-07-14 08:36:00 -0400

    fix(desafios): finalizados exercícios 105 e 106

--- python/03 - estruturas-compostas/desafios/desafio105.py
+++ python/03 - estruturas-compostas/desafios/desafio105.py
@@ -8,7 +8,44 @@ Faça um programa que tenha uma função notas() que pode receber várias notas 
 
 Adicione também a docstrings da função.
 '''
+def linha():
+    return print(f'==' * 15)
 
 def notas(*n, sit=False):
-    ...
+    '''
+    Função para registrar várias notas de uma turma.
+    
+    *n:
+    - Inserir e agrupar incontáveis números.
+    
+    sit=False
+    - Imprime a situação da turma (Reprovada ou Aprovada) caso verdadeiro. False por padrão.
+    '''
 
+    turma = {} # Iniciando um dicionário
+    turma['Notas'] = n
+    
+    tamanho = len(turma['Notas'])
+    maior_nota = max(turma['Notas'])
+    menor_nota = min(turma['Notas'])
+    media_turma = (sum(turma['Notas']) / tamanho) 
+        
+    print(f'Dicionário Completo: {turma}')
+    print(f'Quantidade de notas: {tamanho}')
+    print(f'Maior Nota: {maior_nota}')
+    print(f'Menor Nota: {menor_nota}')
+    print(f'Média da Turma: {media_turma}')
+    
+    if sit == True:
+        if media_turma > 7:
+            return print(f'Turma Aprovada!!')
+        else:
+            return print(f'Turma Reprovada!')
+
+# Programa principal
+
+notas(5,4,3,1,2,10,10,10, sit=False)
+linha()
+notas(5,4,3,1,2,10,10,10, sit=True)
+linha()
+notas(9,9,9,4,7, sit=True)
--- python/03 - estruturas-compostas/desafios/desafio106.py
+++ python/03 - estruturas-compostas/desafios/desafio106.py
@@ -4,3 +4,24 @@ Quando o usuário digitar a palavra 'FIM' o programa se encerrará.
 
 OBS: use cores
 '''
+def escreva(texto):
+    tamanho = len(texto) + 4
+    print("~" * tamanho)
+    print(f'  {texto}')
+    print("~" * tamanho)
+
+def pyhelp(comando):
+    help(comando)
+    
+def loop():
+    while True:
+        escreva("Sistema de ajuda PyHELP")
+        duvida = str(input(f'Digite uma função ou biblioteca: '))
+        
+        if duvida.lower() == 'fim':
+            break
+        else:
+            escreva(f'Acessando o manual Pyhelp da função/biblioteca {duvida}')
+            pyhelp(duvida)
+            
+loop()
