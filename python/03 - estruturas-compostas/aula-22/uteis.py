ef030f98029b58c48132456ac83a68c8dfaba922
luiirye <borg.esluii01645@gmail.com>
2026-07-14 10:40:54 -0400

    Aula 22 (incompleta)

---
+++ python/03 - estruturas-compostas/aula-22/uteis.py
@@ -0,0 +1,15 @@
+'''
+Neste arquivo, terão várias funções exemplos para modularização
+'''
+
+def fatorial(n):
+    f = 1
+    for c in range(1, n+1):
+        f *= c
+    return f
+
+def dobro(n):
+    return n * 2
+
+def triplo(n):
+    return n * 3
