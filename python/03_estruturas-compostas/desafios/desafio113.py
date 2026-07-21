f2357d09a5454121f0469c010763a6275714db16
luiirye <borg.esluii01645@gmail.com>
2026-07-21 10:37:26 -0400

    Exercício 113 não finalizado

--- python/03_estruturas-compostas/desafios/desafio113.py
+++ python/03_estruturas-compostas/desafios/desafio113.py
@@ -3,3 +3,31 @@ Reescreva a função leiaInt() que fizemos no desafio 104.
 Incluindo agora a possibilidade da digitação de um número de tipo inválido.
 Aproveite e crie também uma função leiaFloat() com a mesma funcionalidade.
 '''
+
+def leiaInt(msg):
+    valor = 0
+    while True:
+        num = str(input(msg))
+        if num.isnumeric():
+            valor = int(num)
+            break
+        else:
+            print('\033[31mERRO! Digite um número inteiro válido.\033[m')
+    return valor
+
+
+def leiaFloat(msg):
+    valor = 0
+    while True:
+        num = str(input(msg))
+        if num:
+            valor = float(num)
+            break
+        else:
+            print('\033[31mERRO! Digite um número inteiro válido.\033[m')
+    return valor
+
+# Programa Principal
+n1 = leiaInt(f'Digite um número inteiro: ')
+n2 = leiaFloat(f'Digite um número real: ')
+print(f'Você digitou o número inteiro {n1}\nE o número real: {n2}')
