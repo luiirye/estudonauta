8dc6ebaf407d0ad3ed286fb1ff7e4ccf4f09469d
Luiirye <borg.esluii01645@gmail.com>
2026-08-24 18:00:27 -0400

    Exercício 17 (incompleto)

--- python/mundo_04_orientacao_a_objetos/desafios/d017.py
+++ python/mundo_04_orientacao_a_objetos/desafios/d017.py
@@ -1,10 +1,15 @@
-from rich import print
+from rich import panel
 
 class Produto:
     
-    def  __init__(self, nome_produto, preco_produto):
+    def  __init__(self, nome_produto = "", preco_produto = 0):
         self.produto = nome_produto # Atributo para nome do produto
         self.preco = preco_produto # Atributo para preço do produto
     
-    def Etiqueta(self):
+    def etiqueta_produto(self):
+        return panel("[purple]{self.produto}[/purple]")
+    
+    
+teste = Produto("Teclado", 120)
+print(teste.etiqueta_produto())
         
