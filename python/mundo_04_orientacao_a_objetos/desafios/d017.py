1882e12bcd8fa86108872802a52eb91f122448ae
Luiirye <borg.esluii01645@gmail.com>
2026-08-25 10:42:19 -0400

    d017(finalizado)

--- python/mundo_04_orientacao_a_objetos/desafios/d017.py
+++ python/mundo_04_orientacao_a_objetos/desafios/d017.py
@@ -1,15 +1,24 @@
-from rich import panel
+from rich.panel import Panel
+from rich import print
 
 class Produto:
     
-    def  __init__(self, nome_produto = "", preco_produto = 0):
+    def  __init__(self, nome_produto = "", preco_produto = 0.0):
         self.produto = nome_produto # Atributo para nome do produto
         self.preco = preco_produto # Atributo para preço do produto
     
     def etiqueta_produto(self):
-        return panel("[purple]{self.produto}[/purple]")
+        
+        mensagem = (
+            f'[bold blue]{self.produto}[/bold blue]: R$ [violet]{self.preco:.2f}[/violet]'
+        )
+
+        return Panel(mensagem, title ='Produto' , width=30, style="violet")
     
+p1 = Produto("teclado", 120)
+print(p1.etiqueta_produto())
+p2 = Produto("PC Gamer",10_000)
+print(p2.etiqueta_produto())
     
-teste = Produto("Teclado", 120)
-print(teste.etiqueta_produto())
+
         
---
+++ python/mundo_04_orientacao_a_objetos/desafios/d018.py
@@ -0,0 +1,6 @@
+from rich.panel import Panel
+from rich import print
+
+class Churrasco:
+    def __init__(self):
+        pass
